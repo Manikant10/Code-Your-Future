99757f2144cd0a68df10a298fd2cf3002486c1d9
Manikant10 <manikantraju123@gmail.com>
2024-12-23 11:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -666,8 +666,8 @@ def predict():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5001))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=50001)
 
 
 
